2fa879f78747c9952d80ba895a5d53e588fa91f1
Eduardo Luz <eduardo.luz@edreamsodigeo.com>
2022-09-26 22:47:36 +0100

    adding UITableViewDataSource protocol to render itens on TableView statically

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -11,13 +11,27 @@ protocol AdicionaRefeicaoDelegate {
     func add(_ refeicao: Refeicao)
 }
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UITableViewDataSource {
+    // MARK: - Atributes
     var delegate: RefeicoesTableViewController?
-
+    var itens: [String] = ["Brasil", "EUA"]
+    // MARK: - IBOutlets
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet var caloriaTextField: UITextField?
-
+    
+    //MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        celula.textLabel?.text = itens[indexPath.row]
+        return celula
+    }
+    
+    
+    // MARK: - IBActions
     @IBAction func adicionar(_ sender: Any) {
         guard let nomeRefeicao = nomeTextField?.text else {
             print("guard nomeRefeicao")
